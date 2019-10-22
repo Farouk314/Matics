c81062b2ad6bc41becfd5a39e7fec79887e9cf9f
Farouk314 <fchousein@gmail.com>
2019-10-22 22:28:36 +0100

    Updates

---
+++ precache-manifest.f5bb23e8a1c98980204f341df075f56d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e858ebc40548a9e00a23ccd20f62399c",
+    "url": "/matics/index.html"
+  },
+  {
+    "revision": "2de22aba73217c3f68e0",
+    "url": "/matics/static/css/main.8f761c90.chunk.css"
+  },
+  {
+    "revision": "81082c180662cf406a00",
+    "url": "/matics/static/js/2.f33fe3fa.chunk.js"
+  },
+  {
+    "revision": "2de22aba73217c3f68e0",
+    "url": "/matics/static/js/main.8f775052.chunk.js"
+  },
+  {
+    "revision": "767d4f50203e513c8301",
+    "url": "/matics/static/js/runtime-main.b2d0579f.js"
+  }
+]);
