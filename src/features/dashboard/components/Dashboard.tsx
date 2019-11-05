e1f50bebaed2779dc2a7d567181872478f7c3b9b
Farouk314 <fchousein@gmail.com>
2019-11-05 00:17:44 +0000

    WIP: Dashboard

--- src/features/dashboard/components/Dashboard.tsx
+++ src/features/dashboard/components/Dashboard.tsx
@@ -4,6 +4,7 @@ import { users, userValid } from "helpers/auth";
 import sha256 from "sha256";
 import { localStorage } from "helpers/auth";
 import { State } from "App";
+import { useSpring, animated } from "react-spring";
 
 type DashboardProps = {
   state: State;
@@ -12,6 +13,24 @@ type DashboardProps = {
 type Props = DashboardProps & RouteComponentProps;
 
 const Dashboard: React.FC<Props> = ({ state }) => {
+  const [hovered1, setHovered1] = React.useState(false);
+  const [hovered2, setHovered2] = React.useState(false);
+  const contentProps1 = useSpring({
+    // opacity: hovered ? 1 : 0,
+    // marginTop: hovered ? 0 : -500
+    height: hovered1 ? "100%" : "80%",
+    width: hovered1 ? "100%" : "80%",
+    fontSize: hovered1 ? "1.6rem" : "1.3rem"
+  });
+
+  const contentProps2 = useSpring({
+    // opacity: hovered ? 1 : 0,
+    // marginTop: hovered ? 0 : -500
+    height: hovered2 ? "100%" : "80%",
+    width: hovered2 ? "100%" : "80%",
+    fontSize: hovered2 ? "1.6rem" : "1.3rem"
+  });
+
   React.useEffect(() => {
     if (!userValid()) {
       navigate("/Matics/login");
@@ -43,8 +62,17 @@ const Dashboard: React.FC<Props> = ({ state }) => {
       <div className="CardList">
         <ul>
           <Link to="/Matics/equation-generator">
-            <li>
-              <span>Equation Generator</span>
+            <li onMouseEnter={() => setHovered1(true)} onMouseLeave={() => setHovered1(false)}>
+              <animated.div className="Inner" style={contentProps1}>
+                <span>Equation Generator</span>
+              </animated.div>
+            </li>
+          </Link>
+          <Link to="/Matics/equation-generator">
+            <li onMouseEnter={() => setHovered2(true)} onMouseLeave={() => setHovered2(false)}>
+              <animated.div className="Inner" style={contentProps2}>
+                <span>Equation Generator</span>
+              </animated.div>
             </li>
           </Link>
         </ul>
